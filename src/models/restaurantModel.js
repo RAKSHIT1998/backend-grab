feb7ae01080f1da64bc74206e17505b2b88e9fbc
RAKSHIT1998 <84240184+RAKSHIT1998@users.noreply.github.com>
2025-07-23 23:16:03 +0530

    Create restaurantModel.js

---
+++ src/models/restaurantModel.js
@@ -0,0 +1,54 @@
+// src/models/restaurantModel.js
+import mongoose from "mongoose";
+
+const restaurantSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    ownerName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    phone: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    isApproved: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    menu: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Menu",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+export default Restaurant;
