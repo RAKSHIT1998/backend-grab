b253d9225ee0102c5570acaca1480cbaa5be0c95
RAKSHIT1998 <84240184+RAKSHIT1998@users.noreply.github.com>
2025-07-26 15:02:14 +0530

    Add application settings feature

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ import movieRouter from './src/routes/movieRoutes.js';
 import activityRouter from './src/routes/activityRoutes.js';
 import newsRouter from './src/routes/newsRoutes.js';
 import depositRouter from './src/routes/depositRoutes.js';
+import settingRouter from './src/routes/settingRoutes.js';
 
 // Mount API routes
 app.use('/api/users', userRouter);
@@ -67,6 +68,7 @@ app.use('/api/movies', movieRouter);
 app.use('/api/activities', activityRouter);
 app.use('/api/news', newsRouter);
 app.use('/api/deposits', depositRouter);
+app.use('/api/settings', settingRouter);
 
 // Root health check route
 app.get('/', (req, res) => {
