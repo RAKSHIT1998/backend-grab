aca3dc6613de3bba0036ea81705aa79317ca4193
RAKSHIT1998 <84240184+RAKSHIT1998@users.noreply.github.com>
2025-07-28 13:41:40 +0530

    Fix Mongoose model overwrite by consolidating User model

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import Restaurant from '../models/restaurant.js';
-import User from '../models/User.js';
+import User from '../models/userModel.js';
 import Order from '../models/Order.js';
 import Ride from '../models/Ride.js';
 
